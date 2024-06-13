39a40e93f94ac5589295ee8f7705eff20327d774
Lucas Queiroz <Lucas92113@gmail.com>
2024-06-13 13:09:00 -0300

    Delete Python - Senai/Aulas directory

--- Python - Senai/Aulas/Aula03.04_Condições-Simples/Desafio003.py
+++
@@ -1,7 +0,0 @@
-#     DESAFIO 3
-    
-dkm = float(input('Qual foi a distancia percorrida em Km/h:'))
-if dkm<=200 :
-    print(f'o percurso percorrico fica no total de R$ {dkm*0.50}')
-else: 
-    print(f'ao passar de 200(Km/h) o percurso percorrico fica no total de R$ {dkm*0.45}')
